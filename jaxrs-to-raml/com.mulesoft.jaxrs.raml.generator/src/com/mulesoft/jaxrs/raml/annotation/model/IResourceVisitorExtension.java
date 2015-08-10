eff91f1b6af15b456a4375264da768908b831183
KonstantinSviridov <ksviridov@gmail.com>
2015-08-10 15:13:18 +0500

    Some comments for resource visitor extensions.

--- jaxrs-to-raml/com.mulesoft.jaxrs.raml.generator/src/com/mulesoft/jaxrs/raml/annotation/model/IResourceVisitorExtension.java
+++ jaxrs-to-raml/com.mulesoft.jaxrs.raml.generator/src/com/mulesoft/jaxrs/raml/annotation/model/IResourceVisitorExtension.java
@@ -2,6 +2,11 @@ package com.mulesoft.jaxrs.raml.annotation.model;
 
 public interface IResourceVisitorExtension {
 	
+	/**
+	 * Decide if the type needs schema to be generated
+	 * @param type
+	 * @return Whether to generate schema or not
+	 */
 	boolean generateSchema(ITypeModel type);
 
 }
--- jaxrs-to-raml/com.mulesoft.jaxrs.raml.generator/src/com/mulesoft/jaxrs/raml/jaxb/ISchemaModelBuilderExtension.java
+++ jaxrs-to-raml/com.mulesoft.jaxrs.raml.generator/src/com/mulesoft/jaxrs/raml/jaxb/ISchemaModelBuilderExtension.java
@@ -8,20 +8,24 @@ import com.mulesoft.jaxrs.raml.annotation.model.IResourceVisitorExtension;
 public interface ISchemaModelBuilderExtension extends IResourceVisitorExtension {
 	
 	/**
-	 * Adjust property right after it and its type have been created by the SchemaModelBuilder.If prop is null
-	 * @param prop adjusted or created property property. By
+	 * The method allows to adjust property right after it and its type have been created by the
+	 * SchemaModelBuilder.
+	 * @param prop Property created by the SchemaModelBuilder. May be null.
+	 * @return Adjusted or created property. By default must return @prop, not null.
 	 */
 	ISchemaProperty processProperty(JAXBProperty jaxbProp, ISchemaProperty prop);
 	
 	/**
 	 * Adjust type right after it and all its properties have been created by the SchemaModelBuilder
-	 * @param type type
+	 * The method is called once for each type.
+	 * @param type Type created by the SchemaModelBuilder
 	 */
 	void processType(ISchemaType type);
 	
 	/**
 	 * Adjust schema type right after it and all its properties have been created by the SchemaModelBuilder
-	 * @param type type
+	 * The method is called only for root type.
+	 * @param type Root type created by the SchemaModelBuilder
 	 */
 	void processModel(ISchemaType type);
 

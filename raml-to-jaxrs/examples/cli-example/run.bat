2c0068555b84472d058594a3c38ffbee45aec6eb
Alexander Osipov <inet.shu+1@gmail.com>
2015-12-16 19:00:19 +0600

    add scripts for cli example

--- raml-to-jaxrs/examples/cli-example/run.bat
+++ raml-to-jaxrs/examples/cli-example/run.bat
@@ -1,1 +1,10 @@
-java -cp ..\..\core\target\raml-jaxrs-codegen-core-1.3.4-SNAPSHOT-jar-with-dependencies.jar org.raml.jaxrs.codegen.core.Launcher -basePackageName com.somecompany.sample -outputDirectory output -sourceDirectory raml
+@echo off
+
+if exist ..\..\core\target\raml-jaxrs-codegen-core-1.3.4-SNAPSHOT-jar-with-dependencies.jar (
+  java -cp ..\..\core\target\raml-jaxrs-codegen-core-1.3.4-SNAPSHOT-jar-with-dependencies.jar org.raml.jaxrs.codegen.core.Launcher -basePackageName com.somecompany.sample -outputDirectory output -sourceDirectory raml
+) else (
+  echo *** run mvn assembly:assembly -DdescriptorId=jar-with-dependencies
+  echo *** in raml-to-jaxrs/core first to make jar
+)
+
+
---
+++ raml-to-jaxrs/examples/cli-example/run.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo "*** run mvn assembly:assembly -DdescriptorId=jar-with-dependencies"
+echo "*** in raml-to-jaxrs/core first to make jar"
+
+java -cp ..\..\core\target\raml-jaxrs-codegen-core-1.3.4-SNAPSHOT-jar-with-dependencies.jar org.raml.jaxrs.codegen.core.Launcher -basePackageName com.somecompany.sample -outputDirectory output -sourceDirectory raml
+
+

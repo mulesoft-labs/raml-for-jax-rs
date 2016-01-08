baad0ecf79aea8a843b5192bd32e19c5d093ae13
Alexander Osipov <inet.shu+1@gmail.com>
2016-01-08 23:12:34 +0600

    fix versions to 1.3.4

--- raml-to-jaxrs/examples/cli-example/run.bat
+++ raml-to-jaxrs/examples/cli-example/run.bat
@@ -1,7 +1,7 @@
 @echo off
 
-if exist ..\..\core\target\raml-jaxrs-codegen-core-1.3.4-SNAPSHOT-jar-with-dependencies.jar (
-  java -cp ..\..\core\target\raml-jaxrs-codegen-core-1.3.4-SNAPSHOT-jar-with-dependencies.jar org.raml.jaxrs.codegen.core.Launcher -basePackageName com.somecompany.sample -outputDirectory output -sourceDirectory raml
+if exist ..\..\core\target\raml-jaxrs-codegen-core-1.3.4-jar-with-dependencies.jar (
+  java -cp ..\..\core\target\raml-jaxrs-codegen-core-1.3.4-jar-with-dependencies.jar org.raml.jaxrs.codegen.core.Launcher -basePackageName com.somecompany.sample -outputDirectory output -sourceDirectory raml
 ) else (
   echo *** run mvn assembly:assembly -DdescriptorId=jar-with-dependencies
   echo *** in raml-to-jaxrs/core first to make jar

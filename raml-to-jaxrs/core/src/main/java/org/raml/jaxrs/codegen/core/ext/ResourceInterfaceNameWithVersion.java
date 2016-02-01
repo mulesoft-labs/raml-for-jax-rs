07c38211c209327f19f0bab8f0f33b71269129a6
jeroenvere <jeroen.verellen@basware.com>
2016-02-01 13:54:24 +0100

    make version check less strict

--- raml-to-jaxrs/core/src/main/java/org/raml/jaxrs/codegen/core/ext/ResourceInterfaceNameWithVersion.java
+++ raml-to-jaxrs/core/src/main/java/org/raml/jaxrs/codegen/core/ext/ResourceInterfaceNameWithVersion.java
@@ -6,25 +6,21 @@ import org.raml.model.Resource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.Optional;
-import java.util.regex.Pattern;
-
 public class ResourceInterfaceNameWithVersion extends AbstractGeneratorExtension implements InterfaceNameBuilderExtension {
     private static final Logger LOGGER = LoggerFactory.getLogger(ResourceInterfaceNameWithVersion.class);
-    private static final Pattern VERSION_PATTERN = Pattern.compile("^v[0-9]+$");
 
     @Override
     public String buildResourceInterfaceName(Resource resource) {
         Configuration config = new Configuration();
         config.setInterfaceNameSuffix("Resource");
         String resourceName = Names.buildResourceInterfaceName(resource, config);
-        Optional<String> version = Optional.ofNullable(getRaml().getVersion());
-        if (version.isPresent() && VERSION_PATTERN.matcher(version.get()).matches()) {
-            resourceName += version.get().toUpperCase();
+        String version = getRaml().getVersion();
+        if (version != null) {
+            resourceName += version.toUpperCase();
             LOGGER.debug("Generated resource name: " + resourceName);
             return resourceName;
         }
-        String message = "Invalid version found in RAML: " + version;
+        String message = "No version found in RAML.";
         LOGGER.warn(message);
         throw new IllegalArgumentException(message);
     }

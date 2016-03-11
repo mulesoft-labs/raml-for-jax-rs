c4cec19a56ac3e5eef0eb08194807b948f2316cd
Alexander Osipov <inet.shu+1@gmail.com>
2016-03-11 21:17:05 +0600

    fix cli example jar version

--- raml-to-jaxrs/examples/cli-example/run.sh
+++ raml-to-jaxrs/examples/cli-example/run.sh
@@ -3,6 +3,6 @@
 echo "*** run mvn assembly:assembly -DdescriptorId=jar-with-dependencies"
 echo "*** in raml-to-jaxrs/core first to make jar"
 
-java -cp ..\..\core\target\raml-jaxrs-codegen-core-1.3.4-jar-with-dependencies.jar org.raml.jaxrs.codegen.core.Launcher -basePackageName com.somecompany.sample -outputDirectory output -sourceDirectory raml
+java -cp ..\..\core\target\raml-jaxrs-codegen-core-1.3.5-SNAPSHOT-jar-with-dependencies.jar org.raml.jaxrs.codegen.core.Launcher -basePackageName com.somecompany.sample -outputDirectory output -sourceDirectory raml
 
 

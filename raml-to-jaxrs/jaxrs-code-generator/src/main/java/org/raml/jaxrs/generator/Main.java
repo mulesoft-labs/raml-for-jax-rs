0c01c9c18f48228827d15a152044e2c138f1d26c
Jean-Philippe Belanger <jean-philippe.belanger@ericsson.com>
2016-11-10 22:34:08 -0500

    Sort of plugin setup.
    Change-Id: I29aa57382053e54ac986dda5c1780ea8129e3051

---
+++ raml-to-jaxrs/jaxrs-code-generator/src/main/java/org/raml/jaxrs/generator/Main.java
@@ -0,0 +1,16 @@
+package org.raml.jaxrs.generator;
+
+import java.io.File;
+import java.io.IOException;
+
+/**
+ * Created by Jean-Philippe Belanger on 11/10/16.
+ * Just potential zeroes and ones
+ */
+public class Main {
+
+    public static void main(String[] args) throws IOException, GenerationException {
+        RamlScanner scanner = new RamlScanner(args[1], args[2]);
+        scanner.handle(new File(args[0]).toURL());
+    }
+}

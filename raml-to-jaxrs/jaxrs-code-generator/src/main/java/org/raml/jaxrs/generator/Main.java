0d2615431e84d38af99e328a7d7ceeb6cac61df7
Jean-Philippe Belanger <jean-philippe.belanger@ericsson.com>
2016-12-26 18:17:57 -0500

    Args, cli and rest.
    Change-Id: I15039a78ddecd9c3e383f055157b2e5ae49dcc11

--- raml-to-jaxrs/jaxrs-code-generator/src/main/java/org/raml/jaxrs/generator/Main.java
+++ raml-to-jaxrs/jaxrs-code-generator/src/main/java/org/raml/jaxrs/generator/Main.java
@@ -1,7 +1,14 @@
 package org.raml.jaxrs.generator;
 
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
+
 import java.io.File;
 import java.io.IOException;
+import java.util.List;
 
 /**
  * Created by Jean-Philippe Belanger on 11/10/16.
@@ -9,8 +16,38 @@ import java.io.IOException;
  */
 public class Main {
 
-    public static void main(String[] args) throws IOException, GenerationException {
-        RamlScanner scanner = new RamlScanner(args[1], args[2]);
-        scanner.handle(new File(args[0]).toURI().toURL(), args.length <= 3 ? "." : args[3]);
+    public static void main(String[] args) throws IOException, GenerationException, ParseException {
+
+        Options options = new Options();
+        options.addOption("m", "model", true, "model package");
+        options.addOption("s", "support", true, "support package");
+        options.addOption("r", "resource", true, "resource package");
+        options.addOption("e", "extensions", true, "extension options");
+        options.addOption("d", "directory", true, "generation directory");
+
+        CommandLineParser parser = new DefaultParser();
+        CommandLine command = parser.parse(options, args);
+        String modelDir = command.getOptionValue("m");
+        String supportDir = command.getOptionValue("s");
+        String resourceDir = command.getOptionValue("r");
+        String directory = command.getOptionValue("d");
+
+        if ( modelDir == null ) {
+            modelDir = resourceDir;
+        }
+
+        if ( supportDir == null ) {
+
+            supportDir = resourceDir;
+        }
+
+        List<String> ramlFiles =  command.getArgList();
+
+        RamlScanner scanner = new RamlScanner(directory, resourceDir, modelDir, supportDir );
+
+        for (String ramlFile : ramlFiles) {
+
+            scanner.handle(new File(ramlFile));
+        }
     }
 }

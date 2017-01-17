77dfc9495180cdda011f68a285d0d83df4b37d38
Jean-Philippe Belanger <jean-philippe.belanger@ericsson.com>
2017-01-16 19:40:59 -0500

    Quick fix for hooks
    Change-Id: I6ddb2c473fea822c6342afeddff70fa8d0abe2b6

--- raml-to-jaxrs/jaxrs-code-generator/src/main/java/org/raml/jaxrs/generator/extension/Context.java
+++ raml-to-jaxrs/jaxrs-code-generator/src/main/java/org/raml/jaxrs/generator/extension/Context.java
@@ -13,7 +13,24 @@ public interface Context {
     String getModelPackage();
     String getSupportPackage();
 
+    /**
+     * Rename a method defined in a JavaPoet method builder.
+     *
+     * This creates an identical method builder with a new name.
+     * @param builder
+     * @param name
+     * @return
+     */
     MethodSpec.Builder rename(MethodSpec.Builder builder, String name);
+
+    /**
+     * Rename a class/interface defined in a JavaPoet type builder.
+     *
+     * This creates an identical method builder with a new name.
+     * @param builder
+     * @param name
+     * @return
+     */
 
     TypeSpec.Builder rename(TypeSpec.Builder builder, String name);
 }
--- raml-to-jaxrs/jaxrs-code-generator/src/test/java/org/raml/jaxrs/generator/v10/AnnotationsTest.java
+++ raml-to-jaxrs/jaxrs-code-generator/src/test/java/org/raml/jaxrs/generator/v10/AnnotationsTest.java
@@ -1,14 +1,20 @@
 package org.raml.jaxrs.generator.v10;
 
 import org.junit.Test;
+import org.raml.jaxrs.generator.builders.extensions.resources.TrialResourceExtension;
+import org.raml.jaxrs.generator.builders.extensions.resources.TrialResourceMethodExtension;
+import org.raml.jaxrs.generator.builders.extensions.resources.TrialResponseClassExtension;
+import org.raml.jaxrs.generator.builders.extensions.resources.TrialResponseMethodExtension;
 import org.raml.v2.api.RamlModelBuilder;
 import org.raml.v2.api.RamlModelResult;
 import org.raml.v2.api.model.common.ValidationResult;
+import org.raml.v2.api.model.v10.api.Api;
 import org.raml.v2.api.model.v10.datamodel.TypeDeclaration;
 
 import java.io.InputStreamReader;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 /**
  * Created by Jean-Philippe Belanger on 1/2/17.
@@ -31,6 +37,17 @@ public class AnnotationsTest {
 
     }
 
+    @Test
+    public void getResponseClass() throws Exception {
+
+        Api type = buildApi(this, "annotations.raml", 0);
+
+        assertTrue(Annotations.ON_RESOURCE_CLASS_CREATION.get(type.resources().get(0)) instanceof TrialResourceExtension );
+        assertTrue(Annotations.ON_METHOD_CREATION.get(type.resources().get(0).methods().get(0)) instanceof TrialResourceMethodExtension);
+        assertTrue(Annotations.ON_RESPONSE_CLASS_CREATION.get(type.resources().get(0).methods().get(0)) instanceof TrialResponseClassExtension);
+        assertTrue(Annotations.ON_RESPONSE_METHOD_CREATION.get(type.resources().get(0).methods().get(0).responses().get(0)) instanceof TrialResponseMethodExtension);
+    }
+
     public static TypeDeclaration buildType(Object test, String raml, int index) {
         RamlModelResult ramlModelResult = new RamlModelBuilder()
                 .buildApi(new InputStreamReader(test.getClass().getResourceAsStream(raml)), ".");
@@ -41,6 +58,19 @@ public class AnnotationsTest {
             throw new AssertionError();
         } else {
             return ramlModelResult.getApiV10().types().get(index);
+        }
+    }
+
+    public static Api buildApi(Object test, String raml, int index) {
+        RamlModelResult ramlModelResult = new RamlModelBuilder()
+                .buildApi(new InputStreamReader(test.getClass().getResourceAsStream(raml)), ".");
+        if (ramlModelResult.hasErrors()) {
+            for (ValidationResult validationResult : ramlModelResult.getValidationResults()) {
+                System.out.println(validationResult.getMessage());
+            }
+            throw new AssertionError();
+        } else {
+            return ramlModelResult.getApiV10();
         }
     }
 

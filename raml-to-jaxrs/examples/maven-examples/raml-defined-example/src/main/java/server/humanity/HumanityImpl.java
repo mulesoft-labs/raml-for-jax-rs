7c8b5293ce62d5b71b11fced6a4dea1068e171f1
Jean-Philippe Belanger <jean-philippe.belanger@ericsson.com>
2017-02-12 15:44:24 -0500

    Fixes example, prettier.
    Change-Id: I3c0c73db064b199aa65702540f92f9849b9be339

--- raml-to-jaxrs/examples/maven-examples/raml-defined-example/src/main/java/server/humanity/HumanityImpl.java
+++ raml-to-jaxrs/examples/maven-examples/raml-defined-example/src/main/java/server/humanity/HumanityImpl.java
@@ -20,26 +20,51 @@ import example.model.CorpseImpl;
 import example.model.Gender;
 import example.model.Human;
 import example.model.Limb;
+import example.model.Person;
 import example.model.PersonImpl;
-import example.types.Humanity;
+import example.types.Humans;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Date;
+import java.util.List;
 
 /**
  * Created by Jean-Philippe Belanger on 11/11/16. Just potential zeroes and ones
  */
-public class HumanityImpl implements Humanity {
-
+public class HumanityImpl implements Humans {
 
   @Override
-  public PutHumanityResponse putHumanity(Human entity) {
-    return PutHumanityResponse.respond200();
+  public GetHumansResponse getHumans(String type) {
+    List<Human> humans = new ArrayList<>();
+    PersonImpl pi = new PersonImpl();
+    pi.setGender(Gender.FEMALE);
+    pi.setWeight(180);
+
+    pi.setDateOfBirth(new Date());
+    pi.setInstantOfBirth(new Date());
+    pi.setTimeOfArrival(new Date());
+    pi.setDateOfBirth(new Date());
+    pi.setTimeOfBirth(new Date());
+    pi.setRequestTime(new Date());
+
+    CorpseImpl ci = new CorpseImpl();
+    ci.setDateOfDeath(new Date());
+    pi.setSiblings(Collections.<Human>singletonList(ci));
+    pi.setLimbs(new Limb(new ArmImpl()));
+
+    CorpseImpl another = new CorpseImpl();
+    another.setGender(Gender.OTHER);
+    another.setDateOfDeath(new Date());
+
+    humans.add(pi);
+    humans.add(another);
+
+    return GetHumansResponse.respond200WithApplicationJson(humans);
   }
 
   @Override
-  public GetHumanityResponse getHumanity(String type) {
-
+  public GetHumansByIdResponse getHumansById(String id, String type) {
     if ("person".equals(type)) {
 
       PersonImpl pi = new PersonImpl();
@@ -57,13 +82,18 @@ public class HumanityImpl implements Humanity {
       ci.setDateOfDeath(new Date());
       pi.setSiblings(Collections.<Human>singletonList(ci));
       pi.setLimbs(new Limb(new ArmImpl()));
-      return GetHumanityResponse.respond200WithApplicationJson(pi);
+      return GetHumansByIdResponse.respond200WithApplicationJson(pi);
     } else {
 
       CorpseImpl ci = new CorpseImpl();
       ci.setGender(Gender.OTHER);
       ci.setDateOfDeath(new Date());
-      return GetHumanityResponse.respond200WithApplicationJson(ci);
+      return GetHumansByIdResponse.respond200WithApplicationJson(ci);
     }
+  }
+
+  @Override
+  public PutHumansByIdResponse putHumansById(String id, Human entity) {
+    return null;
   }
 }

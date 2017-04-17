2a4151a80b1b2555549dc0264ec08f5728fbc41f
Jean-Philippe Belanger <jean-philippe.belanger@ericsson.com>
2017-04-17 14:47:17 -0400

    cli for raml.
    Change-Id: Ic4297df2d9f2d002d4be1f8f79a5755b35ad5757

--- jaxrs-to-raml/jaxrs-to-raml-cli/src/main/java/org/raml/jaxrs/cli/Main.java
+++ jaxrs-to-raml/jaxrs-to-raml-cli/src/main/java/org/raml/jaxrs/cli/Main.java
@@ -15,40 +15,95 @@
  */
 package org.raml.jaxrs.cli;
 
+import com.google.common.base.Function;
+import com.google.common.collect.Collections2;
+import com.google.common.collect.FluentIterable;
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.HelpFormatter;
+import org.apache.commons.cli.Option;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
 import org.raml.jaxrs.converter.RamlConfiguration;
 import org.raml.jaxrs.raml.core.DefaultRamlConfiguration;
 import org.raml.jaxrs.raml.core.OneStopShop;
 
+import javax.annotation.Nullable;
 import java.lang.annotation.Annotation;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Collections;
+import java.util.List;
 
 public class Main {
 
   public static void main(String[] args) throws Exception {
-    Path jaxRsResourceFile = Paths.get(args[0]);
-    Path ramlOutputFile = Paths.get(args[1]);
 
-    Path jaxRsSourceRoot = null;
-    if (args.length > 2) {
-      jaxRsSourceRoot = Paths.get(args[2]);
+    Options options = new Options();
+    options.addOption(Option.builder("a").required().longOpt("applicationDirectory").hasArg().desc("application name").build());
+    options.addOption(Option.builder("o").required().longOpt("output").hasArg().desc("RAML output file").build());
+    options.addOption("s", "sourceRoot", true, "JaxRs source root");
+    options.addOption("t", "translatedAnnotations", true, "translated annotation list (comma separated");
+
+    try {
+
+      CommandLineParser parser = new DefaultParser();
+      CommandLine command = parser.parse(options, args);
+
+      Path jaxRsResourceFile = Paths.get(command.getOptionValue('a'));
+      Path ramlOutputFile = Paths.get(command.getOptionValue('o'));
+
+      Path jaxRsSourceRoot = null;
+      if (command.hasOption('s')) {
+
+        jaxRsSourceRoot = Paths.get(command.getOptionValue('s'));
+      }
+
+
+      RamlConfiguration ramlConfiguration =
+          DefaultRamlConfiguration.forApplication(jaxRsResourceFile.getFileName().toString(),
+                                                  Collections.<Class<? extends Annotation>>emptySet());
+
+      OneStopShop.Builder builder =
+          OneStopShop.builder().withJaxRsClassesRoot(jaxRsResourceFile)
+              .withRamlOutputFile(ramlOutputFile).withRamlConfiguration(ramlConfiguration);
+
+      if (null != jaxRsSourceRoot) {
+        builder.withSourceCodeRoot(jaxRsSourceRoot);
+      }
+
+      if (command.hasOption('t')) {
+
+        String[] classes = command.getOptionValue('t').split(",");
+        List<Class<? extends Annotation>> c =
+            FluentIterable.of(classes).transform(
+                                                 new Function<String, Class<? extends Annotation>>() {
+
+                                                   @Nullable
+                                                   @Override
+                                                   public Class<? extends Annotation> apply(@Nullable String input) {
+
+                                                     try {
+                                                       return (Class<? extends Annotation>) Class.forName(input);
+                                                     } catch (ClassNotFoundException e) {
+                                                       throw new IllegalArgumentException(
+                                                                                          "while building translated annotations list",
+                                                                                          e);
+                                                     }
+                                                   }
+                                                 }).toList();
+
+        builder.withTranslatedAnnotations(c);
+      }
+      OneStopShop oneStopShop = builder.build();
+
+      oneStopShop.parseJaxRsAndOutputRaml();
+    } catch (ParseException e) {
+      HelpFormatter formatter = new HelpFormatter();
+      System.err.println(e.getMessage());
+      formatter.printHelp("jaxrstoraml", options, true);
+
     }
-
-    RamlConfiguration ramlConfiguration =
-        DefaultRamlConfiguration.forApplication(jaxRsResourceFile.getFileName().toString(),
-                                                Collections.<Class<? extends Annotation>>emptySet());
-
-    OneStopShop.Builder builder =
-        OneStopShop.builder().withJaxRsClassesRoot(jaxRsResourceFile)
-            .withRamlOutputFile(ramlOutputFile).withRamlConfiguration(ramlConfiguration);
-
-    if (null != jaxRsSourceRoot) {
-      builder.withSourceCodeRoot(jaxRsSourceRoot);
-    }
-
-    OneStopShop oneStopShop = builder.build();
-
-    oneStopShop.parseJaxRsAndOutputRaml();
   }
 }
